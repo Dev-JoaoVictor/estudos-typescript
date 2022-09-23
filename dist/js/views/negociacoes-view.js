08560fc086cf4ed40109a9b6f970528eb460c169
João <joaovictor.ramos34@gmail.com>
2022-09-22 23:00:20 -0300

    criando a tabela

--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,15 +1,23 @@
 export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
     template() {
         return `
-        <table class=" table table-hover table-bordered"
+        <table class="table table-hover table-bordered"
             <thead>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
             </thead>
             <tbody>
             </tbody>
         </table>
         `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
     }
 }
