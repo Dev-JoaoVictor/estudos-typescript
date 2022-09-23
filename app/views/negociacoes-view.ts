8c1cde18e95706ac7a29a7a32bd90cebdb906219
João <joaovictor.ramos34@gmail.com>
2022-09-22 21:18:30 -0300

    criando o template da tabela

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,15 @@
+export class NegociacoesView {
+    template(): string {
+        return `
+        <table class=" table table-hover table-bordered"
+            <thead>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        `
+    }
+}
