5e680f6eb45ae5ba606aa28a476a94cc49307f58
Dev-JoaoVictor <joaovictor.ramos34@gmail.com>
2022-10-06 14:35:05 -0300

    conclusão do curso

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,12 +1,18 @@
-import { Imprimivel } from "../utils/imprimivel.js";
+import { Modelo } from '../interfaces/modelo.js';
 
-export class Negociacao extends Imprimivel {
+export class Negociacao implements Modelo<Negociacao> {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
-    ) {
-        super();
+    ) {}
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 
     get volume(): number {
@@ -22,15 +28,13 @@ export class Negociacao extends Imprimivel {
         return `
             Data: ${this.data},
             Quantidade: ${this.quantidade},
-            Valor: ${this.valor},
+            Valor: ${this.valor}
         `;
     }
-    
-    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-        const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
-        const quantidade = parseInt(quantidadeString);
-        const valor = parseFloat(valorString);
-        return new Negociacao(date, quantidade, valor);
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear();
     }
 }
